2c74bf9b74f0176a678cd6e10f57a9dee6891242
KahhorovSh04 <shakhzodbekkakhkhorov04@gmail.com>
2022-09-23 11:15:27 +0500

    m

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import Navbar from './Navbar'
-import Search from './Search'
 import Chats from './Chats'
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
       <Navbar />
-      {/* <Search/> */}
       <Chats />
     </div>
   )
