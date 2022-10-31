4c47fe1d74d4cafb05554974283bc865cb7c57aa
KahhorovSh04 <shakhzodbekkakhkhorov04@gmail.com>
2022-10-31 15:08:41 +0800

    Updating lastMessage when message deleted

--- src/components/MessageMenu.jsx
+++ src/components/MessageMenu.jsx
@@ -1,12 +1,14 @@
 import { useContext } from 'react'
-import { updateDoc, doc } from 'firebase/firestore'
+import { updateDoc, doc, serverTimestamp } from 'firebase/firestore'
 import { db } from '../firebase'
 import { ChatContext } from '../context/ChatContext'
 import { MessagesContext } from '../context/MessagesContext'
+import { AuthContext } from '../context/AuthContext'
 import { AiOutlineDelete } from 'react-icons/ai'
 
 const MessageMenu = ({ uid }) => {
   const { data } = useContext(ChatContext)
+  const { currentUser } = useContext(AuthContext)
   const context = useContext(MessagesContext)
   const messages = context.messages.messages
   const dispatch = context.dispatch
@@ -19,6 +21,22 @@ const MessageMenu = ({ uid }) => {
       updateDoc(docRef, { messages: updatedMessages })
         .then(() => {
           dispatch({ type: 'SET_MESSAGES', payload: updatedMessages })
+        })
+        .then(() => {
+          updateDoc(doc(db, 'userChats', currentUser.uid), {
+            [data.chatId + '.lastMessage']: {
+              text: updatedMessages[updatedMessages.length - 1].text,
+            },
+            [data.chatId + '.date']: serverTimestamp(),
+          })
+        })
+        .then(() => {
+          updateDoc(doc(db, 'userChats', data.user.uid), {
+            [data.chatId + '.lastMessage']: {
+              text: updatedMessages[updatedMessages.length - 1].text,
+            },
+            [data.chatId + '.date']: serverTimestamp(),
+          })
           console.log('success')
         })
         .catch((err) => {
