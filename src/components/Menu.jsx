25952bd315070a5d008b8d58d596ffe1d57e97e2
KahhorovSh04 <shakhzodbekkakhkhorov04@gmail.com>
2022-11-04 13:34:18 +0800

    Logout and edit mode added to Menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,18 +1,42 @@
-import React, { useContext } from 'react'
-import { signOut } from 'firebase/auth'
+import { useContext, useState, useRef } from 'react'
+import { signOut, updateProfile } from 'firebase/auth'
 import { auth } from '../firebase'
 import { AuthContext } from '../context/AuthContext'
 import { IoClose } from 'react-icons/io5'
 import { MdOutlineModeEdit } from 'react-icons/md'
+import { MdDone } from 'react-icons/md'
 
 const Menu = ({ open, setOpen }) => {
   const { currentUser } = useContext(AuthContext)
+  const [editMode, setEditMode] = useState(false)
+  const nameRef = useRef(null)
+
+  const handleClick = () => {
+    const name = nameRef.current
+
+    if (name.innerText) {
+      updateProfile(auth.currentUser, { displayName: name.innerText })
+        .then(() => {
+          setEditMode(false)
+          alert('Your name changed succesfully')
+        })
+        .catch((err) => {
+          console.error(err)
+          alert(err.message)
+        })
+    } else {
+      name.innerText = currentUser.displayName
+      alert('Your name must iclude at least a character')
+    }
+  }
 
   return (
     <div className='menuContainer' style={{ display: open ? 'flex' : 'none' }}>
       <div className='menu'>
         <div className='actions'>
-          <button className='logout'>Logout</button>
+          <button className='logout' onClick={() => signOut(auth)}>
+            Logout
+          </button>
           <button className='close' onClick={() => setOpen(false)}>
             <IoClose />
           </button>
@@ -20,11 +44,32 @@ const Menu = ({ open, setOpen }) => {
         <div className='profile'>
           <img src={currentUser.photoURL} alt='Profile' />
           <div>
-            <span className='name'>{currentUser.displayName}</span>
+            <span
+              className='name'
+              ref={nameRef}
+              contentEditable={editMode}
+              suppressContentEditableWarning
+            >
+              {currentUser.displayName}
+            </span>
           </div>
-          <button className='edit'>
-            <MdOutlineModeEdit />
-          </button>
+
+          <div className='profileActions'>
+            <button
+              className='edit'
+              onClick={() => setEditMode(true)}
+              style={{ display: editMode ? 'none' : 'block' }}
+            >
+              <MdOutlineModeEdit />
+            </button>
+            <button
+              className='set'
+              onClick={handleClick}
+              style={{ display: editMode ? 'block' : 'none' }}
+            >
+              <MdDone />
+            </button>
+          </div>
         </div>
       </div>
     </div>
